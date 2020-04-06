bbc9cf6d2ba96f2ac29ff7fd063b37aa377c8d19
Van Bui <van.bt@trans-cosmos.com.vn>
2020-04-06 17:11:46 +0700

    2020-04-06 Truyen tham so vao ham

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
 
 export default class Product extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+  
+  onAddToCart = () => {
+    alert(this.props.name +' - '+ this.props.price);
+  }
+
+  //có tham số truyền vào
+  onAddToCart2 = (text) => {
+    alert(text);
+  }
+
   render() {
     
     return (
@@ -12,7 +26,8 @@ export default class Product extends Component {
             <p className="card-text">{this.props.price}</p>
           </div>
         </div>
-        <button type="button" className="btn btn-primary">Mua hàng</button>
+        <button type="button" className="btn btn-primary" onClick={this.onAddToCart}>Mua hàng</button>
+        <button type="button" className="btn btn-primary" onClick={() => this.onAddToCart2('ABC')}>Mua hàng 2</button>
       </div>
       
     )
